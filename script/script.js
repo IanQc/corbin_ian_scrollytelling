5807595b92e3e7e3dc55ce4c6aa0b8fb647bc3ed
IanQc <ian.corbin.qc@gmail.com>
2024-05-05 22:24:28 -0400

    2.3

--- script/script.js
+++ script/script.js
@@ -1,8 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(MotionPathPlugin);
 gsap.registerPlugin(TextPlugin);
-
-
+gsap.registerPlugin(DrawSVGPlugin);
 
 
 /*------------Intro------------ */ 
@@ -16,7 +15,7 @@ timeline1.to(".titreAnim", {
     ease: "slow.in",
     text: "Soldat de l'espace",
     fontFamily: "Hell",
-    color:"#ffe436"
+    color:"#ffe436",
 })
 .to(".soustitre01", {
     duration: 2,
@@ -71,7 +70,7 @@ scrollTrigger: {
 }
 })
     
-gsap.to('.header.stars4', { 
+gsap.to('header .stars4', { 
 y: '100%',
 scrollTrigger: {
     scrub: true,
@@ -82,6 +81,16 @@ scrollTrigger: {
 }
 })
 
+gsap.to('#Deep_Space .stars1', { 
+    y: '5%',
+    backgroundPosition: `50% ${-innerHeight / 2}px`,
+    scrollTrigger: {
+        scrub: true,
+        markers: true,
+        trigger: '#Deep_Space',
+    }
+    })
+/*
 gsap.to('#Deep_Space.stars4', { 
     y: '100%',
     scrollTrigger: {
@@ -92,7 +101,7 @@ gsap.to('#Deep_Space.stars4', {
         trigger: '.comete',
     }
     })
-    
+    */
 gsap.to('.stars5', { 
 y: '50%',
 scrollTrigger: {
@@ -115,16 +124,23 @@ gsap.to('.pod', {
     })
 
 
-gsap.to('.flamme', { 
-    opacity:1,
-    scrollTrigger: {
-        scrub: true,
-        markers: true,
-        start: 'top',
-        end: 'bottom 20%',
-        trigger: '#orbit',
-    }
-    })
+    gsap.to('.flamme', { 
+        opacity: 1,
+        scrollTrigger: {
+            scrub: true,
+            markers: true,
+            start: 'top',
+            end: 'bottom 20%',
+            trigger: '#orbit',
+            onEnter: function() {
+                document.querySelector('.flamme').style.animationPlayState = 'running';
+            },
+            onLeaveBack: function() {
+                document.querySelector('.flamme').style.animationPlayState = 'paused';
+            }
+        }
+    });
+    
 
 
     
@@ -155,11 +171,18 @@ gsap.to(".stars3", {
 
 
 
-// Création d'une timeline GSAP
-let timeline2 = gsap.timeline();
+
 
 // Ajout de l'animation à la timeline
-timeline2.to('.comete', { 
+gsap.timeline({scrollTrigger: {
+    //scrub: true,
+   //pin:true,
+    //markers:true,
+    trigger: "#Deep_Space", // Élément qui déclenche l'animation
+    start: "top -40%", 
+    end: "bottom",
+}})
+.to('.comete', { 
     opacity: 1,
     x: "110vw",
     y: "90vh",
@@ -197,35 +220,271 @@ timeline2.to('.comete', {
 });
 
 
-ScrollTrigger.create({
-    trigger: "#DeepSpace", // Élément qui déclenche l'animation
-    start: "top ", 
-    end: "bottom", 
-    animation: timeline2 
+
+
+//gsap.fromTo('.satellite', {x: "-40vw"}, {x: "110vw", repeat:-1, ease:"power1.out", delay: 2 , duration:20});
+
+gsap.to(".satellite", {
+    x: "110vw", 
+    
+    ease:"power1.out", 
+
+    scrollTrigger: {
+        trigger: ".satellite",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+        markers: true
+    }
 });
 
-gsap.fromTo('.satellite', {x: "-40vw"}, {x: "110vw", repeat:-1, ease:"power1.out", delay: 2 , duration:20});
+gsap.to(".planet3", {
+    y: "400%",
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#Deep_Space",
+      scrub: true
+    }
+});
 
-gsap.fromTo('.nuage.quatre', {x: "250%"}, {x: "-600%", repeat:-1, ease:"power1.out", delay: 2 , duration:10});
-gsap.fromTo('.nuage.six', {x: "200%"}, {x: "-800%", repeat:-1, ease:"power1.out", duration:14});
-gsap.fromTo('.nuage.un', {x: "200%"}, {x: "-600%", repeat:-1, ease:"power1.out",delay: 4, duration:20});
-gsap.fromTo('.nuage.neuf', {x: "200%"}, {x: "-600%", repeat:-1, ease:"power1.out", duration:15});
-gsap.fromTo('.nuage.onze', {x: "200%"}, {x: "-800%", repeat:-1, ease:"power1.out",delay: 3, duration:15});
 
-gsap.fromTo('.nuage.deux', {x: "-200%"}, {x: "600%", repeat:-1, ease:"power1.out", duration:10});
+    
+
+
+gsap.to(".planet4", {
+    y: "-1000%",
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#Deep_Space",
+      scrub: true
+    }
+});
+
+gsap.to('.pod2', {
+    transformOrigin: '50% 50%',
+    motionPath: {
+      path: [
+        {x: "0vw", y: "25vh"}, 
+        {x: "75vw", y: "375vh"}
+      ],
+    },
+    duration: 8,
+    repeat: -1,
+    scrollTrigger: {
+        trigger: "#troposphere",
+      }
+  })
+
+
+/*
+
+gsap.to(section.bg, {
+    backgroundPosition: `50% ${-innerHeight / 2}px`,
+    ease: "none",
+    scrollTrigger: {
+      trigger: section,
+      scrub: true
+    }
+  });*/
+
+/*
+gsap.to(".nuage.quatre", {
+    x: "-600%", 
+    ease:"power1.out", 
+    scrollTrigger: {
+        trigger: ".nuage.quatre",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+        markers: true
+    }
+});
+
+gsap.to(".nuage.six", {
+    x: "-800%", 
+    ease:"power1.out", 
+    scrollTrigger: {
+        trigger: ".nuage.six",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true,
+        markers: true
+    }
+});
+*/
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#exosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['.nuages .nuage.un', '.nuages .nuage.quatre', '.nuages .nuage.six', '.nuages .nuage.neuf'], { 
+    x: "-150vw", 
+    repeat: -1, 
+    duration: 12,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#exosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['.nuages .nuage.deux', '.nuages .nuage.trois', '.nuages .nuage.cinque', '.nuages .nuage.huit', '.nuages .nuage.dix'], { 
+    x: "150vw", 
+    repeat: -1, 
+    duration: 13,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#thermosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#thermosphere .nuage.un', '#thermosphere .nuage.quatre', '#thermosphere .nuage.six', '#thermosphere .nuage.neuf', '#thermosphere .nuage.onze'], { 
+    x: "-150vw", 
+    repeat: -1, 
+    duration: 12,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#thermosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#thermosphere .nuage.deux', '#thermosphere .nuage.trois', '#thermosphere .nuage.cinque', '#thermosphere .nuage.huit', '#thermosphere .nuage.dix', '#thermosphere .nuage.douze', '#thermosphere .nuage.treize'], { 
+    x: "150vw", 
+    repeat: -1, 
+    duration: 13,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#mesosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#mesosphere .nuage.un', '#mesosphere .nuage.quatre'], { 
+    x: "-150vw", 
+    repeat: -1, 
+    duration: 12,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#mesosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#mesosphere .nuage.deux', '#mesosphere .nuage.trois', '#mesosphere .nuage.cinque'], { 
+    x: "150vw", 
+    repeat: -1, 
+    duration: 13,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#stratosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#stratosphere .nuage.un', '#stratosphere .nuage.quatre'], { 
+    x: "-150vw", 
+    repeat: -1, 
+    duration: 12,
+});
+
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#stratosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#stratosphere .nuage.deux', '#stratosphere .nuage.trois', '#stratosphere .nuage.cinque'], { 
+    x: "150vw", 
+    repeat: -1, 
+    duration: 13,
+});
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#stratosphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#stratosphere .oiseau.un', '#stratosphere .oiseau.deux', '#stratosphere .oiseau.trois', '#stratosphere .oiseau.quatre', '#stratosphere .oiseau.cinque'], { 
+    x: "120vw", 
+    repeat: -1, 
+    duration:8,
+    stagger: {
+        each: 1, // délai d'1 seconde entre le début de chaque animation
+        from: "start" // commence le décompte à partir du début de chaque animation
+    }
+});
+
+gsap.timeline({scrollTrigger: {
+    trigger: "#troposphere",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true
+}})
+.to(['#troposphere .oiseau.un', '#stratosphere .oiseau.deux', '#stratosphere .oiseau.trois', '#stratosphere .oiseau.quatre', '#stratosphere .oiseau.cinque'], { 
+    x: "120vw", 
+    repeat: -1, 
+    duration:8,
+    stagger: {
+        each: 1, // délai d'1 seconde entre le début de chaque animation
+        from: "start" // commence le décompte à partir du début de chaque animation
+    }
+});
+
+/*
+gsap.fromTo('.oiseau.un', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", duration:8});
+gsap.fromTo('.oiseau.deux', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", delay: 3, duration:5});
+gsap.fromTo('.oiseau.trois', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", duration:10});
+gsap.fromTo('.oiseau.quatre', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", delay: 1, duration:4});
+gsap.fromTo('.oiseau.cinque', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", duration:9});
+
+*/
+/*gsap.to(".nuage.un", {
+    x: "-600%", 
+    repeat:-1,
+    ease:"power1.out",  
+    duration:10,
+    scrollTrigger: {
+        trigger: "#exosphere",
+        start: "top bottom",
+        end: "bottom top",
+        markers: true
+    }
+});*/
+
+
+//gsap.fromTo('.nuage.quatre', {x: "250%"}, {x: "-600%", repeat:-1, ease:"power1.out", delay: 2 , duration:10});
+//gsap.fromTo('.nuage.six', {x: "200%"}, {x: "-800%", repeat:-1, ease:"power1.out", duration:14});
+//gsap.fromTo('.nuage.un', {x: "200%"}, {x: "-600%", repeat:-1, ease:"power1.out",delay: 4, duration:20});
+//gsap.fromTo('.nuage.neuf', {x: "200%"}, {x: "-600%", repeat:-1, ease:"power1.out", duration:15});
+//gsap.fromTo('.nuage.onze', {x: "200%"}, {x: "-800%", repeat:-1, ease:"power1.out",delay: 3, duration:15});
+
+/*gsap.fromTo('.nuage.deux', {x: "-200%"}, {x: "600%", repeat:-1, ease:"power1.out", duration:10});
 gsap.fromTo('.nuage.trois', {x: "-200%"}, {x: "600%", repeat:-1, ease:"power1.out", delay: 6, duration:14});
 gsap.fromTo('.nuage.cinque', {x: "-300%"}, {x: "600%", repeat:-1, ease:"power1.out", duration:12});
 gsap.fromTo('.nuage.huit', {x: "-400%"}, {x: "400%", repeat:-1, ease:"power1.out",delay: 2, duration:15});
 gsap.fromTo('.nuage.dix', {x: "-200%"}, {x: "600%", repeat:-1, ease:"power1.out", duration:15});
 gsap.fromTo('.nuage.douze', {x: "800%"}, {x: "-700%", repeat:-1, ease:"power1.out", delay: 3, duration:16});
 gsap.fromTo('.nuage.treize', {x: "-200%"}, {x: "700%", repeat:-1, ease:"power1.out", duration:10});
+*/
 
-
-gsap.fromTo('.oiseau.un', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", duration:8});
-gsap.fromTo('.oiseau.deux', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", delay: 3, duration:5});
-gsap.fromTo('.oiseau.trois', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", duration:10});
-gsap.fromTo('.oiseau.quatre', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", delay: 1, duration:4});
-gsap.fromTo('.oiseau.cinque', {x: "-200%"}, {x: "110vw", repeat:-1, ease:"power1.out", duration:9});
 
 
 
